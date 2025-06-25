f66efa0756b45d587a635bb0410400baa0bf5259
dilusaper <ludikemare1004@gmail.com>
2025-06-25 12:50:53 -0500

    Lectura del potenciómetro en porcentaje hecha la calibración

--- tareas/main/tareas.c
+++ tareas/main/tareas.c
@@ -17,39 +17,87 @@ Crear tarea que lea el UART y permita cambiar el threshold de cualquiera de los 
 #include "freertos/task.h"
 #include "esp_log.h"
 #include "esp_adc/adc_oneshot.h"
+#include "esp_adc/adc_cali.h"
 
-#define ADC_BANDWIDTH       ADC_BITWIDTH_12
+
+#define ADC_UNIT            ADC_UNIT_1
+#define ADC_RESOLUTION      ADC_BITWIDTH_12
 #define ADC_ATTEN           ADC_ATTEN_DB_12
 #define POT_CHANNEL         ADC_CHANNEL_5
+#define MAX_VOLTAJE_MV      3204
 
-void app_main(void)
-{
+adc_oneshot_unit_handle_t adc_handle = NULL;
+adc_cali_handle_t cali_handle = NULL;
+bool do_calibration = false;
+
+// Estrucuta de control de entrada del ADC
+adc_oneshot_chan_cfg_t config = {
+    .bitwidth = ADC_RESOLUTION,
+    .atten = ADC_ATTEN,
+};
+
+void iniciar_adc (void) {
     // Estructura del manejador del ADC
-    adc_oneshot_unit_handle_t adc_handle;
     adc_oneshot_unit_init_cfg_t init_config = {
         .unit_id = ADC_UNIT_1,               // Selección del ADC
     };
     adc_oneshot_new_unit(&init_config, &adc_handle);    // Uniendo configuración y manejador
+}
 
-    // Estrucuta de control de entrada del ADC
-    adc_oneshot_chan_cfg_t config = {
-        .bitwidth = ADC_BANDWIDTH,
-        .atten = ADC_ATTEN,
-    };
+// Función para conocer el esquema de calibración de la placa
+void esquemas_calibracion(void){
+    adc_cali_scheme_ver_t scheme_mask;
+    esp_err_t ret = adc_cali_check_scheme(&scheme_mask);
+
+    if (ret == ESP_OK) {
+        printf("Esquemas disponibles: \n");
+        if (scheme_mask & ADC_CALI_SCHEME_VER_LINE_FITTING) {
+            printf(" - Line \n");   // Según mis pruebas, este no está disponible en mi placa
+        }
+        else if (scheme_mask & ADC_CALI_SCHEME_VER_CURVE_FITTING) {
+            printf(" - Curve \n");
+            adc_cali_curve_fitting_config_t cali_config = {
+                .unit_id = ADC_UNIT,
+                .atten = ADC_ATTEN,
+                .bitwidth = ADC_RESOLUTION,
+                .chan = POT_CHANNEL,
+            };
+
+            if (adc_cali_create_scheme_curve_fitting(&cali_config, &cali_handle) == ESP_OK) {
+                do_calibration = true;
+            }
+        }
+    }
+    else {
+        printf("Error al verificar esquemas de calibración: %s", esp_err_to_name(ret));
+    }
+}
+
+void lectura_potenciometro (void *arg) {
+    
+    iniciar_adc();
 
     // Enlace del control de entrada con el manejador
     adc_oneshot_config_channel(adc_handle, POT_CHANNEL, &config);
-    int pot_raw;            // Variable para la lectura del adc
 
+    esquemas_calibracion();
     while (1)
     {
-        esp_err_t ret = adc_oneshot_read(adc_handle, POT_CHANNEL, &pot_raw);        // Lectura del ADC
+        int adc_raw;
+        int mili_volts;  
+        
+
+        esp_err_t ret = adc_oneshot_read(adc_handle, POT_CHANNEL, &adc_raw);
         if (ret == ESP_OK) {
-            printf("ADC raw value: %d\n", pot_raw);
+            if (do_calibration) {
+                if (adc_cali_raw_to_voltage(cali_handle, adc_raw, &mili_volts) == ESP_OK) {
+                    printf("Potenciómetro al %d%%\n", mili_volts * 100 / MAX_VOLTAJE_MV);
+                };
+            }
         }
-        else {
-            printf("ADC error: %s", esp_err_to_name(ret));
-        }
-        vTaskDelay(pdMS_TO_TICKS(200));
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
-}    
+}
+void app_main(void) {
+    xTaskCreate(lectura_potenciometro, "Lectura_Pot", 2048, NULL, 5, NULL);
+}   
